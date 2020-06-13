a9d03cb5f18b26788f881f720bfd21ea2fbdc51b
RB Whitaker <RB.Whitaker@asirobots.com>
2020-06-13 14:36:18 -0600

    Added in the side stepper.

---
+++ MarioBros/Sidestepper.cs
@@ -0,0 +1,18 @@
+﻿namespace MarioBros
+{
+    public class Sidestepper : Monster
+    {
+        private const float SidestepperSpeed = 8;
+
+        private static readonly Dimensions SidestepperPhysicsDimensions = new Dimensions(0.8f, 0.8f, 1.8f, 0);
+
+        private static readonly Dimensions SidestepperVisualDimensions = new Dimensions(1, 1, 2, 0);
+
+        public bool IsAngry { get; private set; }
+
+        public Sidestepper() : base(SidestepperVisualDimensions, SidestepperPhysicsDimensions)
+        {
+            speed = SidestepperSpeed;
+        }
+    }
+}
